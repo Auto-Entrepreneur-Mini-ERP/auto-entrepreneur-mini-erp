ac85407328c219bcff51bced58f48f21f7feac37
Younes AIT SI ABBOU <younesaitsiabou@gmail.com>
2026-02-06 16:55:37 +0100

    Added document module

--- server/src/modules/document/document.service.ts
+++ server/src/modules/document/document.service.ts
@@ -1,23 +1,102 @@
+import { Prisma } from "../../../generated/prisma/client.js";
+import { prisma } from "../../lib/prisma.js";
+import { pagination } from "../../utils/pagination.js";
+import { autoentrepreneurExists } from "../auto-entrepreneur/utils/autoentrepreneurExists.js";
 import type { DocumentCreateInput, DocumentUpdateInput } from "./document.types.js";
+import { documentExists } from "./utils/documentExists.js";
 
-const getAllDocuments = async (autoentrepreneurId: string) => {
+const getAllDocuments = async (autoentrepreneurId: string, page: number, limit: number) => {
+    autoentrepreneurExists(autoentrepreneurId);
 
+    const startIndex = pagination.paginationIndex(page, limit);
+
+    const documents = await prisma.document.findMany({
+        skip: startIndex,
+        take: limit,
+        where:{
+            AutoEntrepreneurId: autoentrepreneurId,
+        }
+    });
+    
+    return documents;
 };
 
-const getOneDocument = async (autoentrepreneurId: string, ) => {
+const getOneDocument = async (autoentrepreneurId: string, documentId: string) => {
+    autoentrepreneurExists(autoentrepreneurId);
+
+    const document = await prisma.document.findUnique({
+        where:{
+            id: documentId,
+            AutoEntrepreneurId: autoentrepreneurId,
+        }
+    });
     
+    return document;
 };
 
 const createDocument = async (autoentrepreneurId: string, data: DocumentCreateInput) => {
-    
+    autoentrepreneurExists(autoentrepreneurId);
+
+    const DocumentInputData: Prisma.DocumentCreateInput = {
+        name: data.name,
+        type: 'pdf',
+        category: data.category || null,
+        fileUrl: data.fileUrl,
+        fileSize: 256,
+        description: data.description || null,
+        AutoEntrepreneur: {
+            connect: {
+                id: autoentrepreneurId
+            }
+        }
+    };
+
+    const newDoc = await prisma.document.create({
+        data: DocumentInputData
+    });
+
+    return newDoc;
 };
 
 const updateDocument = async (autoentrepreneurId: string, documentId: string,  data: DocumentUpdateInput) => {
-    
+    autoentrepreneurExists(autoentrepreneurId);
+    documentExists(documentId);
+
+    const DocumentUpdateData: Prisma.DocumentUpdateInput = {
+        name: data.name,
+        category: data.category || null,
+        fileUrl: data.fileUrl,
+        description: data.description || null,
+        AutoEntrepreneur:{
+            connect:{
+                id: autoentrepreneurId
+            }
+        }
+    };
+
+    const updatedDoc = await prisma.document.update({
+        where: {
+            id: documentId,
+            AutoEntrepreneurId: autoentrepreneurId,
+        },
+        data: DocumentUpdateData
+    });
+
+    return updatedDoc;
 };
 
 const deleteDocument = async (autoentrepreneurId: string, documentId: string) => {
-    
+    autoentrepreneurExists(autoentrepreneurId);
+    documentExists(documentId);
+
+    await prisma.document.delete({
+        where:{
+            id: documentId,
+            AutoEntrepreneurId: autoentrepreneurId
+        }
+    });
+
+    return true;
 };
 
 export const documentService = {
